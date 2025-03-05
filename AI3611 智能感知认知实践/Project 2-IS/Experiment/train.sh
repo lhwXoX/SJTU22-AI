7d90ac32753e19694813e40adb9c2c8c27dd6bd3
lhwXoX <lhw040228@gmail.com>
2025-03-05 12:00:14 +0800

    update

--- AI3611 智能感知认知实践/Project 2-IS/Experiment/train.sh
+++ AI3611 智能感知认知实践/Project 2-IS/Experiment/train.sh
@@ -1,7 +1,7 @@
 # epsilon
-python main.py -save_name 'latent_32_epsilon_0.5' -epsilon 0.5
-python main.py -save_name 'latent_32_epsilon_1.0' -epsilon 1.0
-python main.py -save_name 'latent_32_epsilon_2.0' -epsilon 2.0
+python main.py -save_name 'latent_64_epsilon_0.5' -epsilon 0.5
+python main.py -save_name 'latent_64_epsilon_1.0' -epsilon 1.0
+python main.py -save_name 'latent_64_epsilon_2.0' -epsilon 2.0
 
 # latent dimension
 python main.py -save_name 'latent_1_epsilon_1.0' -d_latent 1
